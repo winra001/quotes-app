7b62a1170f46f448a4cc107fcfa13dfbfe999854
daniel ra <winra001@hotmail.com>
2017-07-08 14:08:23 +1000

    Implementing Tabs Navigation, Using Quotes Data, Using the Ionic "List" and "List Item" Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+import { TabsPage } from "../pages/tabs/tabs";
+import { FavoritesPage } from "../pages/favorites/favorites";
+import { LibraryPage } from "../pages/library/library";
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    FavoritesPage,
+    LibraryPage,
+    TabsPage
   ],
   imports: [
     BrowserModule,
@@ -19,7 +23,9 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    FavoritesPage,
+    LibraryPage,
+    TabsPage
   ],
   providers: [
     StatusBar,
